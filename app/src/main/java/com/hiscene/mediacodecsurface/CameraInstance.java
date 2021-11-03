edbb25e20dd7efe1fc05361058e28f34dc6bf4fe
jigl <ji_gl@126.com>
2021-11-03 17:46:43 +0800

    init surface input mediacodec encoder
    Signed-off-by: jigl <ji_gl@126.com>

---
+++ app/src/main/java/com/hiscene/mediacodecsurface/CameraInstance.java
@@ -0,0 +1,35 @@
+package com.hiscene.mediacodecsurface;
+
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+
+import java.io.IOException;
+
+public class CameraInstance {
+    private Camera mCamera;
+
+    public void open() {
+        int id = 0;
+        mCamera = Camera.open();
+    }
+
+    public void startPreview(SurfaceTexture surfaceTexture) {
+        try {
+            Camera.Parameters parameters = mCamera.getParameters();
+            parameters.setPreviewSize(1280, 720);
+            parameters.setRecordingHint(true);
+            mCamera.setPreviewTexture(surfaceTexture);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mCamera.startPreview();
+    }
+
+    public void stopPreview() {
+        mCamera.stopPreview();
+    }
+
+    public void closeCamera() {
+        mCamera.release();
+    }
+}
